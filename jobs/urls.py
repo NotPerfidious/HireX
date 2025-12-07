ee3fd4569bef3b5376055424f05fa336c2ca6e88
Muhammad Rehan <162461517+mrehankarim@users.noreply.github.com>
2025-12-07 14:25:34 +0500

    Added new apis to sync with frontend

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from .views import (
     Jobs, AddSkill, ApplyJobAPIView, HRApplicationListAPIView,
     ApplicationStatusUpdateAPIView, ScheduleInterviewAPIView,
-    InterviewerFeedbackAPIView, NotificationListAPIView
+    InterviewerFeedbackAPIView, NotificationListAPIView, InterviewsListAPIView
+    , NotificationMarkReadAPIView
 )
+from .views import CandidateApplicationListAPIView
 
 urlpatterns = [
     path("", Jobs.as_view()),              # /jobs/  → GET (list), POST
@@ -14,9 +16,13 @@ urlpatterns = [
 urlpatterns += [
     path('apply/', AddSkill.as_view(), name='apply-placeholder'),
     path('apply-job/', ApplyJobAPIView.as_view(), name='apply-job'),
+    path('interviews/', InterviewsListAPIView.as_view(), name='interviews-list'),
     path('applications/', HRApplicationListAPIView.as_view(), name='hr-applications'),
+    path('my-applications/', CandidateApplicationListAPIView.as_view(), name='candidate-applications'),
     path('applications/<int:app_id>/status/', ApplicationStatusUpdateAPIView.as_view(), name='update-application-status'),
     path('schedule-interview/', ScheduleInterviewAPIView.as_view(), name='schedule-interview'),
     path('interviews/<int:interview_id>/feedback/', InterviewerFeedbackAPIView.as_view(), name='interview-feedback'),
     path('notifications/', NotificationListAPIView.as_view(), name='notifications')
+    ,
+    path('notifications/<int:note_id>/read/', NotificationMarkReadAPIView.as_view(), name='notification-mark-read')
 ]
